386d6ee73ba7d352d73a1a47b99c10ae549a3b53
Yuta-Ogata62 <yuta.ogata62@gmail.com>
2023-05-19 01:35:25 +0900

    add feature to delete

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
 const todoTitleInput = document.querySelector("#todoTitle");
 const todoButtonElement = document.querySelector("#addTodoButton");
-const addedTodoElement = document.querySelector("#addedTodo");
 const todoListElement = document.querySelector("#todoItems");
 
 let todoItems = [];
 let eraseItems = [];
 
-function clickFunction(){
+function clickFunction() {
 
-    todoItems.push(todoTitleInput.value);
+    const todoItem = {
+        title: todoTitleInput.value,
+        id: crypto.randomUUID(),
+    }
+    todoItems.push(todoItem);
 
     todoListElement.innerHTML = ""//reset
 
+    renderFunction();
+
+};
+todoButtonElement.addEventListener("click", clickFunction);
+
+// [{id: "fdd"} , {id: "dsfvd"}] O
+// ["vdfv", "csdvd"] X
+
+function getListItemIndexById(listItems, id) {
+
+    let targetItemIndex = -1;
+    
+    for (let index = 0; index < listItems.length; index++) {
+        const item = listItems[index];
+
+        if(item.id === id) {
+            targetItemIndex = index;
+            return;
+        } 
+    }
+
+    return targetItemIndex;
+}
+ function renderFunction(){
     for (i = 0; i < todoItems.length; i++) {
-        let  item = document.createElement('li');
-        //let  erase = document.createElement('li');
-        const todoTitle = todoItems[i]
-        //const erasebubtton = todoItems[i]
-        item.innerHTML = todoTitle;
-        //erase.innerHTML = erasebubtton;
+    let item = document.createElement('li');
+    const todoItem = todoItems[i];
+    item.innerHTML = todoItem.title;
+
+    item.addEventListener("click", () => {
+        const targetIndex = getListItemIndexById(todoItems, todoItem.id)
+        todoItems.splice(targetIndex, 1);
+        todoListElement.innerHTML = "";
+        renderFunction();
+    })
+
+
     todoListElement.appendChild(item);
-
-    function clearTextBoxes() {
-        const inputs = document.todoTitleInput();
-        const clearInputs = ""
-            }
-
-};
-
-};
-todoButtonElement.addEventListener("click",clickFunction);
-
-//derit
+ }
+ }
 //fix
+
